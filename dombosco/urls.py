f33f3d79086e01d4d3b1e5d6e42f0e61474efaad
Milton Filho <mosfilho@gmail.com>
2014-04-28 17:40:58 -0300

    Adicionado editor de texto no menu, alinhando o site com o menu, configurando os arquivos estaticos e as medias

--- dombosco/urls.py
+++ dombosco/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,4 +10,13 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
+    url(r'^redactor/', include('redactor.urls')),
 )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+		    (r'^media/(?P<path>.*)$', 'django.views.static.serve', 
+			    {'document_root': settings.MEDIA_ROOT}))
+
+
